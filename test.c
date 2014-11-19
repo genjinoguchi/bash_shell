f262fb0e1e8313b1df8c0acaad2bd28cd3d18247
Genji Noguchi <genji.noguchi@gmail.com>
2014-11-19 11:00:06 -0500

    colors

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int color;
+
+  printf("Colors: \x1b[0;36;45m Hurrrrrrrr.\n");
+
+
+}
+/*
+0: reset colors/style
+1: bold
+4: underline
+30 - 37: black, red, green, yellow, blue, magenta, cyan, and white text
+40 - 47: black, red, green, yellow, blue, magenta, cyan, and white background
+*/
