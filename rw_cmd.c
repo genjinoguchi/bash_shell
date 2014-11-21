5fd760c2dcf001fbc44d6271d2c030dde0f9a387
Genji Noguchi <genji.noguchi@gmail.com>
2014-11-21 10:52:05 -0500

    probably won't use rw_cmd for now; use the GNU readline and history libraries.

--- rw_cmd.c
+++ rw_cmd.c
@@ -12,11 +12,12 @@ int store_cmd(char * cmd) {
 
 	fd = open("cmds", 0_WRONLY | 0_CREAT);
 	if (fd) {
-	//Store the function.	
+	//Store the function.
+		write(fd, cmd, sizeof(cmd));
+		printf(" Function stored. Bytes written : %d",sizeof(cmd));
 	} else {
 		printf("Error: %s\n",strerror(errno));
 	}
-
 }
 
 char * get_last_cmd(char * cmd, int num) {
